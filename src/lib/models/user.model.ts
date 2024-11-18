8b7884c62c5d6bd5a224fc04a2e0dafa78924a97
oFallen <telmuun.b05@gmail.com>
2024-11-18 19:09:45 +0800

    Minor detail change

---
+++ src/lib/models/user.model.ts
@@ -0,0 +1,31 @@
+import { Model, Schema, model, models } from "mongoose";
+
+type User = {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role?: "Customer" | "ResortAdmin";
+  favorites: Schema.Types.ObjectId[];
+};
+
+const userSchema = new Schema<User>(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    role: { type: String, enum: ["Customer", "ResortAdmin"] },
+    favorites: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "menu",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const UserModel: Model<User> =
+  models.users || model<User>("users", userSchema);
