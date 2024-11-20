50e5c2c54950a7ae2333892b7b70fa6b217d6812
Telmuun <telmuun.b05@gmail.com>
2024-11-20 10:13:27 +0800

    shad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-// import { auth } from "@/lib/auth";
-
 export default async function Home() {
-  //   const session = await auth();
-  //   if (session) {
-  //     return <p>Signed in as {session?.user?.email}</p>;
-  //   }
-
   return <div>Home page</div>;
 }
