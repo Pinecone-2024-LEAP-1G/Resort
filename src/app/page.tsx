f5752a97034e9d72f5dc426647c1274f2abfbe2d
24LP3558 24LP3558 <24LP3558@admins-iMac.local>
2024-11-20 19:48:58 +0800

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { PriceDetails } from "components/PriceDetails";
 
 export default async function Home() {
-  return (
-    <div>
-      <PriceDetails />
-    </div>
-  );
+  return <div></div>;
 }
