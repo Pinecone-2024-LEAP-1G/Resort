33ac6eb6ea75efa112c38932f3bcc238df313ece
24LP4008 24LP4008 <24LP4008@admins-iMac.local>
2024-11-20 14:13:33 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 // import { auth } from "@/lib/auth";
 
+import Header from "@/components/layout/Header";
+
 export default async function Home() {
   //   const session = await auth();
   //   if (session) {
   //     return <p>Signed in as {session?.user?.email}</p>;
   //   }
 
-  return <div>Home page</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
---
+++ src/components/layout/HeaderModal.tsx
@@ -0,0 +1,51 @@
+import { LogOut } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Profile from "@/public/icon/Profile";
+import { AlignJustify } from "lucide-react";
+
+export function HeaderModal() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="flex items-center gap-3 border rounded-full h-12 p-3">
+          <AlignJustify />
+          <Profile />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-60 flex flex-col gap-2 p-3 rounded-2xl">
+        <DropdownMenuLabel>Sign up</DropdownMenuLabel>
+        <DropdownMenuItem>
+          <span>Log in</span>
+        </DropdownMenuItem>
+        <div>
+          <p className="border border-solid"></p>
+        </div>
+        <DropdownMenuItem>
+          <span>Gift cards</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>Airbnb your home</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>Host an experience</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <span>Help center</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <LogOut />
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
