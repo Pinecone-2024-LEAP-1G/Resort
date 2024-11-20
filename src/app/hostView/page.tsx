e55e5571557c8a677dc6b9b0eb4a8d4b8ff07f50
24LP3215 24LP3215 <24LP3215@admins-iMac.local>
2024-11-20 15:03:46 +0800

    HostViewPage

--- src/app/hostView/page.tsx
+++ src/app/hostView/page.tsx
@@ -1,46 +1,111 @@
-'use client'
+"use client";
 import { TiMessages } from "react-icons/ti";
-import HostViewCard from "@/components/HostViewCard"
-import { Button } from "@/components/ui/button"
 import { GiBackpack } from "react-icons/gi";
+import { Button } from "components/ui/button";
+import HostViewCardSecond from "components/HostViewCardSecond";
+import HostViewCard from "components/HostViewCard";
 
-
-const HostView = () =>{
-    return(
-        <div className="flex mx-auto w-[1200px]">
-            <div className="w-[500px] h-[230px]">
-                <p className="font-bold mt-[40px] mb-[30px]">Meet your Host</p>
-                <div className="w-[320px] h-[230px]">
-                    <HostViewCard />
-                </div>
-                <div className="flex mt-5 mb-5 items-center">
-                    <div>
-                        <TiMessages className="w-[30px] h-[30px] ml-[10px]" />
-                    </div>
-                    <p className="text-[17px] ml-[19px]">Speaks English and Greek</p>
-                </div>
-                <div className="">
-                     <p className="text-s w-[350px]">Speaks English and Greek Speaks English and Greek Speaks English and Greek Speaks English and Greek..</p>
-                     <Button className="w-[154px] h-[48px] font-extrabold text-[19px]"  variant="link">Show more</Button>
-                </div>
-            </div>
-            <div className="mt-[100px] w-[500px]">
-                <p className="font-bold">Co-hosts</p>
-                <div className="flex">
-                <GiBackpack className="w-[35px] h-[35px]"/>
-                    <p>Holihouse</p>
-                </div>
-                <p className="font-bold mt-5">Host details</p>
-                <p className="text-xs mt-5">Responce rate: 100%</p>
-                <p className="text-xs mt-5">Responds within an hour</p>
-                <Button className="bg-black text-white mt-4" variant="outline">Message Host</Button>
-                <p className="text-xs mt-5">Registration number:99117575</p>
-                <div className="border-top-width-8px;">
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
+const HostView = () => {
+  return (
+    <div className="mx-auto flex w-[1200px]">
+      <div className="h-[230px] w-[500px]">
+        <p className="mb-[30px] mt-[40px] font-bold">Meet your Host</p>
+        <div className="h-[230px] w-[320px]">
+          <HostViewCard />
         </div>
-    )
-}
-export default HostView
+        <div className="h-[230px] w-[320px] pt-[45px]">
+          <HostViewCard />
+        </div>
+        <div className="h-[230px] w-[320px] pt-[70px]">
+          <HostViewCardSecond />
+        </div>
+        <Button
+          className="h-[48px] w-[154px] text-[19px] font-extrabold"
+          variant="link"
+        >
+          Show more
+        </Button>
+        <Button
+          className="h-[48px] w-[154px] text-[19px] font-extrabold"
+          variant="link"
+        >
+          Show more
+        </Button>
+      </div>
+      <div className="mt-[100px] w-[500px]">
+        <p className="text-[30px] font-bold">About Niki </p>
+        <div className="mb-5 mt-5 flex items-center">
+          <div>
+            <TiMessages className="ml-[10px] h-[30px] w-[30px]" />
+          </div>
+          <p className="ml-[19px] text-[17px]">Speaks English and Greek</p>
+        </div>
+        <div className="">
+          <p className="text-s w-[350px]">
+            Speaks English and Greek Speaks English and Greek Speaks English and
+            Greek Speaks English and Greek..
+          </p>
+          <Button
+            className="h-[48px] w-[154px] text-[19px] font-extrabold"
+            variant="link"
+          >
+            Show more
+          </Button>
+        </div>
+      </div>
+      <div className="mt-[100px] w-[500px]">
+        <p className="font-bold">Co-hosts</p>
+        <div className="flex">
+          <GiBackpack className="h-[35px] w-[35px]" />
+          <p>Holihouse</p>
+        </div>
+        <p className="mt-5 font-bold">Host details</p>
+        <p className="mt-5 text-xs">Responce rate: 100%</p>
+        <p className="mt-5 text-xs">Responds within an hour</p>
+        <Button className="mt-4 bg-black text-white" variant="outline">
+          Message Host
+        </Button>
+        <p className="mt-5 text-xs">Registration number:99117575</p>
+        <p>
+          Holihouse Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nisi, iure maxime. Qui, velit assumenda ex earum voluptatibus
+          molestiae expedita maxime, quia non dicta minima labore dolorum a
+          illum deleniti.
+        </p>
+        <div>
+          <p className="mt-5 font-bold">Niki’s reviews</p>
+          <div> </div>
+        </div>
+        <div className="flex gap-2">
+          <div className="h-[230px] w-[320px]">
+            <HostViewCardSecond />
+          </div>
+          <div className="h-[230px] w-[320px]">
+            <HostViewCardSecond />
+          </div>
+        </div>
+        <Button
+          className="font-semi h-[48px] w-[154px] text-[19px]"
+          variant="link"
+        >
+          Show all 45 reviews
+        </Button>
+
+        <p className="mt-5 text-xs">
+          Responds within an hour
+          <Button
+            className="font-semi h-[48px] w-[154px] text-xs"
+            variant="link"
+          >
+            Show all 45 reviews
+          </Button>
+        </p>
+        <div className="border-top-width-8px;">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default HostView;
