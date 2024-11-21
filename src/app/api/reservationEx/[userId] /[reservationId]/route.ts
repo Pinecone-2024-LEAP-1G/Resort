dbd96871fb8c61f4bc4b25e0033220c497b5a4c0
muuuza <muzaankhbayr@gmail.com>
2024-11-21 20:46:18 +0800

    userid gaar get hiiiw

--- src/app/api/reservationEx/[userId] /[reservationId]/route.ts
+++ src/app/api/reservationEx/[userId] /[reservationId]/route.ts
@@ -1,6 +1,5 @@
-import { ReservationModel } from "lib/models";
-import { NextRequest } from "next/server";
-import { json } from "stream/consumers";
+// import { ReservationModel } from "lib/models";
+// import { NextRequest } from "next/server";
 
 // export const GET=async({params}:{params:{userId:string, reservationId:string}})=>{
 //   const reservationId = (await params).reservationId
@@ -24,33 +23,42 @@ import { json } from "stream/consumers";
 //    }
 // }
 
-export const GET= async(request:NextRequest)=>{
+// export const GET= async(request:NextRequest)=>{
   
-const searchParams=request.nextUrl.searchParams
-const  reservationId=searchParams.get("reservationId")
-const userId=searchParams.get("userId")
-if (reservationId) {
-  try {
-      const reservation = await ReservationModel.find({
-        _id: reservationId,
-      })
-        .populate("propertyId")
-        .populate("userId");
+// const searchParams=request.nextUrl.searchParams
+// const  reservationId=searchParams.get("reservationId")
+// const userId=searchParams.get("userId")
+// if (reservationId) {
+//   try {
+//       const reservation = await ReservationModel.find({
+//         _id: reservationId,
+//       })
+//         .populate("propertyId")
+//         .populate("userId");
 
-      return Response.json({ reservation: reservation });
-    } catch (error) {
-      return Response.json({ error: error });
-    }}
-  if (userId)
-  {try {
-    const userreservation=await ReservationModel.find({userId:userId})
-      return Response.json({userReservations:userreservation})
-  } catch (error) {
-    return Response.json({error:error})
-  }}
-}
+//       return Response.json({ reservation: reservation });
+//     } catch (error) {
+//       return Response.json({ error: error });
+//     }}
+//   if (userId)
+//   {try {
+//     const userreservation=await ReservationModel.find({userId:userId})
+//       return Response.json({userReservations:userreservation})
+//   } catch (error) {
+//     return Response.json({error:error})
+//   }}
+// }
 
-
+// export const GET= async({params} : {params:{slug:["userId", "reservationId"]}})=>{
+//   const reservationId = (await params).reservationId
+//   const userId=(await params).userId  
+  
+//   try {
+    
+//   } catch (error) {
+    
+//   }
+// }
 
 // reservation
 // export const GET = async (
---
+++ src/app/api/reservationEx/route.ts
@@ -0,0 +1,39 @@
+import { ReservationModel } from "lib/models";
+import { NextRequest } from "next/server";
+
+export const POST = async (request: NextRequest) => {
+  const {
+    checkIn,
+    checkOut,
+    userId,
+    propertyId,
+    adult,
+    children,
+    infants,
+    totalPrice,
+  } = await request.json();
+  try {
+    const reservation = await ReservationModel.create({
+      propertyId,
+      userId,
+      checkIn: checkIn,
+      checkOut: checkOut,
+      adult,
+      children,
+      infants,
+      totalPrice,
+    });
+    return Response.json({ reservation: reservation });
+  } catch (error) {
+    return Response.json({ message: error });
+  }
+};
+
+export const GET = async () => {
+  try {
+    const reservations = await ReservationModel.find();
+    return Response.json({ reservations: reservations });
+  } catch (error) {
+    return Response.json({ error: error });
+  }
+};
