703dca795be58a79445582f08bb9b6556ab3da06
Telmuun <telmuun.b05@gmail.com>
2024-11-21 12:33:51 +0800

    minor file changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,67 @@
 "use client";
 
-import HostView from "./hostView/page";
+import { Categories } from "components/Categories";
+import HomeCard from "components/HomeCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Category } from "components/Category";
+type CategoriesState = Category[];
+type Category = {
+  name: string;
+  icon?: JSX.Element;
+};
+
+const Properties = [
+  {
+    _id: 1,
+    price: 10,
+    images: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
+    ],
+    location: {
+      address: { street: "", state: "", city: "", zipcode: "" },
+      geo: { type: "Point", coordinates: ["37.119560", "-113.409010"] },
+    },
+  },
+];
+
+// const Properties = () => {
+//   const [categories, setCategories] = useState([]);
+//   useEffect(() => {
+//     const getCategories = async () => {
+//       try {
+//         const response = await axios.get(
+//           "http://localhost:3000/api/properties"
+//         );
+//         Response.json({ categories });
+//         setCategories(response.data);
+//       } catch (error) {
+//         Response.json({ error: error });
+//       }
+//     };
+//     getCategories();
+//   }, [categories]);
 
 const Home = () => {
   return (
     <div>
-      <HostView />
+      <Categories />
+      <div className="grid grid-cols-6 gap-5">
+        {Properties.map((property) => {
+          return (
+            <HomeCard
+              key={property._id}
+              price={property.price}
+              images={property.images}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/components/HomeCard.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { TiStar } from "react-icons/ti";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "components/ui/carousel";
+
+type HomeCardProps = {
+  images: string[];
+  price: number;
+  address?: string[];
+  distance?: string;
+  rating?: string;
+};
+
+export default function HomeCard(props: HomeCardProps) {
+  const { images, address, rating, distance, price } = props;
+  return (
+    <div>
+      <Carousel className="w-full">
+        <CarouselContent>
+          {images?.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <div
+                  style={{
+                    backgroundImage: `url(${image})`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                  className="aspect-[20/19] rounded-xl"
+                ></div>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="grid grid-cols-2">
+        <div>{/* <span>state, city</span> */}</div>
+        <div className="flex ml-auto items-center">
+          <TiStar />
+          <p>rate</p>
+        </div>
+      </div>
+      {/* <p>distance</p> */}
+      <div>
+        <span className="text-[15px] font-bold mr-[2px]">${price}</span>
+        <span>night</span>
+      </div>
+    </div>
+  );
+}
