bf7712842efaf1dc68e595a88c5166de64576d3e
Telmuun <telmuun.b05@gmail.com>
2024-11-22 11:29:09 +0800

    minor chnages in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,35 +11,6 @@ type Category = {
   icon?: JSX.Element;
 };
 
-// const properties = [
-//   {
-//     _id: 1,
-//     price: { perNight: 10, cleaningFee: 10, airbnbFee: 10 },
-//     propertyPictures: [
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKPyluMYN48KYcTZavIMbMFum3WIhsQccmrA&s",
-//     ],
-//     location: {
-//       address: { street: "", state: "", city: "", zipcode: "" },
-//       geo: { type: "Point", coordinates: ["37.119560", "-113.409010"] },
-//     },
-//     description:
-//       "Exercitation consectetur esse anim aliquip eu veniam nulla commodo, Nostrud ipsum proident nulla proident.",
-//     guests: 2,
-//     userId: "673d2f3a37267d5d6f2569ab",
-//     categoryId: "",
-//     totalBedrooms: 3,
-//     totalOccupancy: 7,
-//     totalBathrooms: 3,
-//   },
-// ];
-
-// Fetch URL
-
 const Home = () => {
   const [properties, setProperties] = useState<any[]>([]);
 
@@ -54,7 +25,7 @@ const Home = () => {
   console.log(properties);
   return (
     <div>
-      {/* <Categories /> */}
+      <Categories />
       <div className="grid grid-cols-6 gap-5">
         {properties?.map((property) => {
           return (
