a66af10489f4a2d8153aeb5f077f6926b182ff23
Relop9000 <24LP9809@nest.edu.mn>
2024-11-29 13:45:04 +0800

    Host

---
+++ src/app/become-host/structure/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { PropertyStructure } from "@/components/BecomeHost/PropertyStructure";
+
+const Structure = () => {
+  return (
+    <div>
+      <PropertyStructure />
+    </div>
+  );
+};
+
+export default Structure;
