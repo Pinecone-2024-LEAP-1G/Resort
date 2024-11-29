d641cc2fb18ac5437083900c848ff35b241bac23
Relop9000 <24LP9809@nest.edu.mn>
2024-11-29 13:45:04 +0800

    Host

---
+++ src/app/become-host/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { PropertyOverView } from "@/components/BecomeHost/PropertyOverView";
+import { PropertyStructure } from "@/components/BecomeHost/PropertyStructure";
+
+const BecomeHost = () => {
+  return (
+    <div>
+      <PropertyOverView />
+      <PropertyStructure />
+    </div>
+  );
+};
+
+export default BecomeHost;
