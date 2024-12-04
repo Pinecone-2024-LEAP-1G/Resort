edf8de1b552bdbe37547c2834f2aab31401a37c8
Relop9000 <24LP9809@nest.edu.mn>
2024-12-04 09:45:22 +0800

    last

--- src/components/BecomeHost/AboutYourPlace.tsx
+++ src/components/BecomeHost/AboutYourPlace.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { PropertyOverview } from "./PropertyOverView";
 import { PrivacyType } from "./PrivacyType";
 import { PropertyHeader } from "./PropertyHeader";
@@ -25,39 +23,6 @@ export const AboutYourPlace = () => {
     return <PrivacyType />;
   }
 
-=======
-import { PropertyFooter } from "./PropertyFooter";
-=======
-import { PropertyOverview } from "./PropertyOverView";
-import { PrivacyType } from "./PrivacyType";
->>>>>>> 870f41b (BecomeHost)
-import { PropertyHeader } from "./PropertyHeader";
-import { Button } from "../ui/button";
-import { useState } from "react";
-
-export const AboutYourPlace = () => {
-<<<<<<< HEAD
->>>>>>> 7891b27 (HostSetup first commit)
-=======
-  const [step, setStep] = useState<string>("about");
-
-  const handleNext = () => {
-    setStep("next");
-  };
-
-  const handleBack = () => {
-    setStep("back");
-  };
-
-  if (step === "back") {
-    return <PropertyOverview />;
-  }
-
-  if (step === "next") {
-    return <PrivacyType />;
-  }
-
->>>>>>> 870f41b (BecomeHost)
   return (
     <div className="flex min-h-screen flex-col justify-between">
       <PropertyHeader />
@@ -83,10 +48,6 @@ export const AboutYourPlace = () => {
           </video>
         </div>
       </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 870f41b (BecomeHost)
       <div className="mt-12 flex items-center justify-between border-t px-6 py-4">
         <button
           onClick={handleBack}
@@ -103,12 +64,6 @@ export const AboutYourPlace = () => {
           Next
         </Button>
       </div>
-<<<<<<< HEAD
-=======
-      <PropertyFooter />
->>>>>>> 7891b27 (HostSetup first commit)
-=======
->>>>>>> 870f41b (BecomeHost)
     </div>
   );
 };
