3c116c6748b747e5ca04b4b94d259de65a7ba3ee
24LP8078 24LP8078 <24LP8078@admins-iMac-2.local>
2024-12-10 13:45:18 +0800

    branch soliy

--- src/app/api/properties/[propertyId]/route.ts
+++ src/app/api/properties/[propertyId]/route.ts
@@ -1,11 +1,13 @@
 import { connectToMongoDB } from "@/lib/db";
 import { PropertyModel } from "@/lib/models";
-import { NextRequest } from "next/server";
 
 connectToMongoDB();
-export const GET = async (request: NextRequest) => {
-  const searchParams = request.nextUrl.searchParams;
-  const propertyId = searchParams.get("id");
+export const GET = async (
+  request: Request,
+  { params }: { params: Promise<{ propertyId: string }> },
+) => {
+  const propertyId = (await params).propertyId;
+
   try {
     const property = await PropertyModel.findById({ _id: propertyId }).populate(
       { path: "reviewId", populate: { path: "userId" } },
