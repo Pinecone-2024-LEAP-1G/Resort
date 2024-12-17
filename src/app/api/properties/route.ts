2423dc16ee9f222f4bb44861903c42aa5815f31e
24LP8078 24LP8078 <24LP8078@admins-iMac-2.local>
2024-12-17 12:24:30 +0800

    back deer ywjin

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -29,71 +29,36 @@ export const POST = async (request: NextRequest) => {
     userId,
     phoneNumber,
   } = await request.json();
-  const hostExist = await HostModel.findOne({
-    email,
-  });
-
-  if (!hostExist) {
-    const user = await UserModel.findOne({
-      email,
-    });
-    await HostModel.create({
-      name: user?.name,
-      email: user?.email,
-      phoneNumber: phoneNumber,
-      avatar: user?.image,
-    });
-  }
-
-  try {
-    const hostExist = await HostModel.findOne({
-      email,
-    });
-    if (!hostExist) {
-      const user = await UserModel.findOne({
+  if (userId) {
+    try {
+      const properties = await PropertyModel.create({
+        address,
+        description,
+        guests,
+        price,
+        categoryId,
+        propertyPictures,
+        totalBedrooms,
+        totalBathrooms,
         email,
+        cleaningFee,
+        userId,
       });
-      await HostModel.create({
-        name: user?.name,
-        email: user?.email,
-        phoneNumber: phoneNumber,
+
+      const updateUser = await UserModel.findOneAndUpdate(
+        {
+          _id: userId,
+        },
+        { $push: { propertyId: properties?._id } },
+        { new: true },
+      );
+
+      return Response.json({
+        updateUser: updateUser,
+        properties: properties,
       });
+    } catch (error) {
+      return Response.json({ message: error });
     }
-
-    const properties = await PropertyModel.create({
-      address,
-      description,
-      guests,
-      price,
-      categoryId,
-      propertyPictures,
-      totalBedrooms,
-      totalBathrooms,
-      email,
-      cleaningFee,
-      userId,
-    });
-
-    const updatedHost = await HostModel.findOneAndUpdate(
-      {
-        email: email,
-      },
-      { $push: { propertyId: properties?._id } },
-      { new: true },
-    );
-
-    const updateProprty = await PropertyModel.findByIdAndUpdate(
-      { _id: properties?._id },
-      {
-        userId: updatedHost?._id,
-      },
-      { new: true },
-    );
-    return Response.json({
-      updateProprty: updateProprty,
-      updatedHost: updatedHost,
-    });
-  } catch (error) {
-    return Response.json({ message: error });
   }
 };
