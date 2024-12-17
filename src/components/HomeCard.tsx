7ad491b5fe0692e3019b53dd04dba3f2131597a3
muuuza <muzaankhbayr@gmail.com>
2024-12-17 14:00:48 +0800

    aimgiin ner darhad vserne

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -26,7 +26,7 @@ export default function HomeCard(props: HomeCardProps) {
   return (
     <div className="w-full cursor-pointer rounded-xl bg-transparent shadow-lg transition-shadow duration-200">
       <Carousel className="w-full">
-        <CarouselContent onClick={() => router.push(`/property/${propertyId}`)}>
+        <CarouselContent>
           {propertyPictures?.map((propertyPicture, index) => {
             return (
               <CarouselItem key={index}>
@@ -44,15 +44,19 @@ export default function HomeCard(props: HomeCardProps) {
           })}
         </CarouselContent>
       </Carousel>
-      <div className="flex flex-col gap-2 px-1 py-4">
-        <div className="mt-4 gap-2 md:grid-cols-2">
+      <div className="flex flex-col gap-2 px-5 py-4">
+        <div
+          className="mt-4 gap-2 md:grid-cols-2"
+          onClick={() => router.push(`/property/${propertyId}`)}
+        >
           <div>
-            <span className="text-md">{property?.description}</span>
+            <span className="text-lg font-medium">{property?.description}</span>
           </div>
           <div className="ml-auto flex items-center"></div>
         </div>
-        <div className="flex items-center">
-          <span className="mr-1 text-lg font-semibold">
+        <div className="flex items-center gap-3">
+          <span className="mr-1 text-base text-gray-500">Хоногт</span>
+          <span className="mr-1 text-base font-semibold">
             {new Intl.NumberFormat().format(price)}₮
           </span>
         </div>
