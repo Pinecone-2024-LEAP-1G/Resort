65950fc009f2c8118af4175c28db91fc97a294c7
24LP3215 24LP3215 <24LP3215@Tuvshins-iMac.local>
2024-12-18 14:27:23 +0800

    Leftcard

--- src/components/HostView/HostLeftCardRating.tsx
+++ src/components/HostView/HostLeftCardRating.tsx
@@ -19,15 +19,14 @@ export const HostLeftCardRating = ({
     <div className="m-[20px] mt-[40px] items-center justify-center">
       <div>
         <p className="text-2xl font-bold">{reviewCount}</p>
-        <p className="text-xs">Cэтгэгдэл үлдээсэн</p>
+        <p className="text-xs">Нийт Cэтгэгдэл</p>
       </div>
-      <div className="flex gap-2">
-        <p className="text-[10px] font-bold">
+      <div className="text-xs">
+        <div className="flex items-center gap-1 text-2xl font-bold">
           {rating}
-          <p className="h-[14px] w-[14px]">
-            <FaStar />
-          </p>
-        </p>
+          <FaStar className="h-[14px] w-[14px]" />
+        </div>
+        Үнэлгээ
       </div>
     </div>
   );
