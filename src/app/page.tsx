0ef92695909a1e461359e7687cf294ea94d790aa
muuuza <muzaankhbayr@gmail.com>
2024-12-20 00:06:17 +0800

    splice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { useSearchParams } from "next/navigation";
 
 import { PropertyType } from "@/components/Review";
 import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
 
 import { SkeletonHomeCard } from "@/components/Skeletons/SkeletonHomeCard";
 
@@ -23,7 +24,7 @@ const Home = () => {
 
   const adress = searchParams.get("adress");
   const [footerP, setFooterP] = useState<PropertyType[]>();
-
+  const [sliceCard, setSliceCard] = useState(12);
   useEffect(() => {
     const getaddressProperty = async () => {
       try {
@@ -68,9 +69,16 @@ const Home = () => {
   };
 
   const filteredAndSortedProperties = filterProperty
-    ?.filter((filterProperty) => filterProperty?.reviewId?.length >= 0)
+    .filter((filterProperty) => filterProperty?.reviewId?.length >= 0)
     .sort((a, b) => b.reviewId.length - a.reviewId.length);
-
+  const more = () => {
+    if (filteredAndSortedProperties.length > sliceCard)
+      setSliceCard((prev) => prev + 4);
+  };
+  console.log(sliceCard);
+  const finalProperty = filteredAndSortedProperties?.slice(sliceCard);
+  const finalProperty2 = filterProperty2?.slice(sliceCard);
+  console.log(finalProperty);
   return (
     <div className="mb-10 mt-10">
       {adress ? (
@@ -124,34 +132,6 @@ const Home = () => {
           </div>
         </div>
       )}
-
-      <Categories
-        onClick={(id) => changePropertyCategory(id)}
-        allProperties={() => setFilterProperty(properties)}
-      />
-      <div className="grid grow gap-8 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-        {loading
-          ? Array(10)
-              .fill(null)
-              .map((_, index) => <SkeletonHomeCard key={index} />)
-          : filteredAndSortedProperties?.map((property, index) => {
-              if (filterProperty[0].length === 0)
-                return (
-                  <div key={index} className="p-10 text-center">
-                    Таны хайсан утга олдсонгүй.
-                  </div>
-                );
-              else
-                return (
-                  <HomeCard
-                    key={index}
-                    property={property}
-                    propertyId={property?._id}
-                    propertyPictures={[property?.propertyPictures]}
-                  />
-                );
-            })}
-      </div>
     </div>
   );
 };
