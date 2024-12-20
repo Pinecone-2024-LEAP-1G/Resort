0b0c781e8064f297980b9e0c20978e1e2efacf70
muuuza <muzaankhbayr@gmail.com>
2024-12-20 10:37:51 +0800

    move emoji

--- src/components/PropertyReview/View.tsx
+++ src/components/PropertyReview/View.tsx
@@ -51,9 +51,7 @@ export function ReviewProperty({ propertyId, getPropertyById }: Property) {
       <DialogTrigger asChild>
         {!show && (
           <div className="flex flex-col">
-            <p className="w-fit transform animate-spin cursor-pointer text-xl transition-transform duration-1000">
-              😃
-            </p>
+            <p className="w-fit transform animate-moveTrain">😃</p>
             <Button
               className="rounded-lg bg-green-500 p-5 py-2 font-bold text-white"
               variant="outline"
